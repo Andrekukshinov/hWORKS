5c6e98787af72bf9bfbaad872ce02c75bd3f09db
Андрей <andrekukshinov@gmail.com>
2021-02-14 17:12:36 +0300

    first homework editied

--- hw1/luckyTicket.py
+++ hw1/luckyTicket.py
@@ -1,13 +1,13 @@
 def twoTicketsInRow():
     counter = 0
-    digSum = 0
     for runner in range(100000, 999999):
         digits = [int(d) for d in str(runner)]
+        digSum = 0
         # print(digits)
         for digitsRunner in digits:
             digSum += int(digitsRunner)
         if digSum % 7 == 0:
-            counter = counter + 1
+            counter += 1
         else:
             counter = 0
         if counter == 2:
